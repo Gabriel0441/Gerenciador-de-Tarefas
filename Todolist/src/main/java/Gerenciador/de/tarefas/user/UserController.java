258652b3d645b373b6d78bfad39c20e7b4835495
Gabriel Coutinho <bielg8998@gmail.com>
2026-06-14 21:55:41 -0300

    Atualização

---
+++ Todolist/src/main/java/Gerenciador/de/tarefas/user/UserController.java
@@ -0,0 +1,13 @@
+package Gerenciador.de.tarefas.user;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/users")
+public class UserController {
+    
+    public void create() {
+        
+    }
+}
